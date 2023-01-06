39f55fd5052d7fe7908ada921d477dd53bbf9d1e
Maksim Hakimov <hakmaks@yandex.ru>
2023-01-06 15:33:13 +0200

    create project

---
+++ views/genres.py
@@ -0,0 +1,20 @@
+from flask_restx import Resource, Namespace
+
+from dao.model.genre import GenreSchema
+from implemented import genre_service
+
+genre_ns = Namespace("genres")
+
+
+@genre_ns.route("/")
+class GenresView(Resource):
+    def get(self):
+        genres = genre_service.get_all()
+        return GenreSchema(many=True).dump(genres), 200
+
+
+@genre_ns.route("/<int:bid>")
+class GenreView(Resource):
+    def get(self, bid):
+        genre = genre_service.get_one(bid)
+        return GenreSchema().dump(genre), 200
